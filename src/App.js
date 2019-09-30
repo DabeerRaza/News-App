a613f2698bcfe0c9ec0f4c55a01825d8a05e7931
Dabeer <dabeerofficial@gmail.com>
2019-09-30 15:23:57 +0500

    inital

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: []
+    }
+  }
+  componentDidMount() {
+    fetch("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=84e33c3911ac4238af7093e16b77329a")
+      .then((response) => {
+        return response.json();
+      })
+      .then((myJson) => {
+        this.setState({
+          articles: myJson.articles
+        });
+      });
+  }
+  render() {
+    console.log(this.state);
+    return (
+      <div className="App">
+        {this.state.articles.map((item, index) => {
+          return (
+            <div className="container">
+              <div className="row">
+                <h2 style={{ margin: '40px 0px' }} className="col-xl-12">
+                  {item.title}
+                </h2>
+                <div className="col-xl-4">
+                  <img style={{ width: '100%' }} src={item.urlToImage} alt="news" />
+                </div>
+                <div className="col-xl-7">
+                  <p>
+                    {item.content}...<a href={item.url} >read more</a>
+                  </p>
+                  <h5>Author: {(item.author != null) ? item.author : "Anonymous"}</h5>
+                </div>
+              </div>
+            </div>
+          )
+        })
+        }
+      </div >
+    );
+  }
 }
 
 export default App;
